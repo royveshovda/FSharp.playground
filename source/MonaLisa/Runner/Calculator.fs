a5facc29b0d91c121c217be95b223bf959e8540c
Roy Veshovda <roy@veshovda.no>
2014-11-26 08:27:29 +0100

    Calculation code + testdata

---
+++ source/MonaLisa/Runner/Calculator.fs
@@ -0,0 +1,40 @@
+﻿module Calculator
+
+open System
+
+type Point =
+        struct
+            val X:float
+            val Y:float
+            new (x:float,y:float) = { X=x; Y=y }
+        end
+
+type Solution = Point []
+
+//type Evaluation =
+//    struct
+//        val Value:float
+//        val Solution:Solution
+//        new (value:float,solution:Solution) = { Value=value; Solution=solution }
+//    end
+
+let dist (p1:Point) (p2:Point) =
+    (p1.X - p2.X) * (p1.X - p2.X) + (p1.Y - p2.Y) * (p1.Y - p2.Y)
+
+let length (xs:Solution) =
+    let len = xs |> Array.length
+    xs
+    |> Seq.fold (fun (acc,prev) x ->
+        acc + dist x xs.[prev], (prev+1)%len) (0.,len-1)
+    |> fst
+
+//let quality (xs:Solution) = - length xs
+
+let shuffle (rng:Random) xs =
+    let len = xs |> Array.length
+    for i in (len - 1) .. -1 .. 1 do
+        let j = rng.Next(i + 1)
+        let temp = xs.[j]
+        xs.[j] <- xs.[i]
+        xs.[i] <- temp
+    xs
--- source/MonaLisa/Runner/Program.fs
+++ source/MonaLisa/Runner/Program.fs
@@ -1,12 +1,7 @@
 ﻿open System
 open System.IO
+open Calculator
 
-type Point =
-        struct
-            val X:float
-            val Y:float
-            new (x:float,y:float) = { X=x; Y=y }
-        end
 
 
 type TspFilePart =
@@ -42,16 +37,82 @@ let readFile (filename:string) size linesToSkip =
         |> Seq.toArray
     points
     
-let file = ("C:\\TestData\\burma14.tsp", 14, 8)
-//let file = ("C:\\TestData\\mona-lisa100K.tsp", 100000, 6)
+let file = ("..\\..\\..\\burma14.tsp", 14, 8)
+//let file = ("..\\..\\..\\mona-lisa100K.tsp", 100000, 6)
 
 [<EntryPoint>]
 let main argv = 
     argv |> ignore
     let (filename, size, linesToSkip) = file
-    let points = readFile filename size linesToSkip
+    let original = readFile filename size linesToSkip
 
-    printfn "Point read: %i" points.Length
+    let rng = Random ()
+
+    printfn "Number of points read: %i" original.Length
+    let l = length original
+
+    let sol1 = shuffle rng original
+    let len1 = length sol1
+
+    let sol2 = shuffle rng original
+    let len2 = length sol2
+
+    let sol3 = shuffle rng original
+    let len3 = length sol3
+
+    let sol4 = shuffle rng original
+    let len4 = length sol4
+
+    let sol5 = shuffle rng original
+    let len5 = length sol5
+
+    let sol6 = shuffle rng original
+    let len6 = length sol6
+
+    let sol7 = shuffle rng original
+    let len7 = length sol7
+
+    let sol8 = shuffle rng original
+    let len8 = length sol8
+
+    let sol9 = shuffle rng original
+    let len9 = length sol9
+
+    let sol10 = shuffle rng original
+    let len10 = length sol10
+
+    let sol11 = shuffle rng original
+    let len11 = length sol11
+
+    let sol12 = shuffle rng original
+    let len12 = length sol12
+
+    let sol13 = shuffle rng original
+    let len13 = length sol13
+
+    let sol14 = shuffle rng original
+    let len14 = length sol14
+
+    let sol15 = shuffle rng original
+    let len15 = length sol15
+
+
+    printfn "Length original: %f" l
+    printfn "Length solution 1: %f" len1
+    printfn "Length solution 2: %f" len2
+    printfn "Length solution 3: %f" len3
+    printfn "Length solution 4: %f" len4
+    printfn "Length solution 5: %f" len5
+    printfn "Length solution 6: %f" len6
+    printfn "Length solution 7: %f" len7
+    printfn "Length solution 8: %f" len8
+    printfn "Length solution 9: %f" len9
+    printfn "Length solution 10: %f" len10
+    printfn "Length solution 11: %f" len11
+    printfn "Length solution 12: %f" len12
+    printfn "Length solution 13: %f" len13
+    printfn "Length solution 14: %f" len14
+    printfn "Length solution 15: %f" len15
     Console.ReadKey() |> ignore
 
     0 // return an integer exit code
